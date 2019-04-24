9b6ffec24af6bcd14ef309f91d3cc4f7c4bb34db
Reilly <reilly.wood@icloud.com>
2019-04-24 13:45:49 -0700

    cleanup

--- services/transformer.service.ts
+++ services/transformer.service.ts
@@ -1,7 +1,6 @@
 import {Formula} from '../models/formula';
 import {RowData} from '../models/rowdata';
 
-// todo: should clone instead of transforming in place
 export function transform(data: RowData[], formula: Formula): RowData[] {
     // very hacky way to do a deep clone
     let cloned: RowData[] = JSON.parse(JSON.stringify(data));
