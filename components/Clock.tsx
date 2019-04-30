3ee65859d9df2b2803f3de96f6f198db750347b3
Reilly <reilly.wood@icloud.com>
2019-04-29 21:02:26 -0700

    Change clock to UTC

--- components/Clock.tsx
+++ components/Clock.tsx
@@ -1,17 +1,18 @@
 import '../styles/index.css'
 import { useState, useEffect } from 'react';
+import moment from 'moment';
 
 var timerId: NodeJS.Timeout;
 
 function Clock() {
-    var [time, setTime] = useState(new Date(Date.now()));
+    var [time, setTime] = useState(moment.now());
 
     useEffect(() => {
-        timerId = setInterval(() => setTime(new Date(Date.now())), 1000);
+        timerId = setInterval(() => setTime(moment.now()), 1000);
         return () => clearInterval(timerId);
     });
-    return <div >
-        {time.toTimeString()}
+    return <div className="text-sm">
+        {moment(time).utc().format('YYYY-MM-DD HH:mm:ss' + ' (UTC)')}
     </div>
 }
 
