ee406571457f7a59e41a2b8c678a647d6b2267f4
Reilly <reilly.wood@icloud.com>
2019-06-29 10:51:20 -0700

    change notification count

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,7 @@ import HeaderElement from './HeaderElement'
 
 const Header = () => {
   return <div className="mb-4">
-  <HeaderElement href='/' text='Home' extraClasses='mr-2' notificationCount={1}  ></HeaderElement>
+  <HeaderElement href='/' text='Home' extraClasses='mr-2' notificationCount={2}  ></HeaderElement>
   <HeaderElement href='/extractDashboard' text='Extracts' extraClasses='mr-2'></HeaderElement>
   <HeaderElement href='/transformDashboard' text='Transforms' extraClasses='mr-2' ></HeaderElement>
   <HeaderElement href='/new' text='New' extraClasses='mr-2'  ></HeaderElement>
